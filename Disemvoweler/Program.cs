f88eddee34c065636b97c4bb4d7a9e897e9ddabb
Colin Pierce <cpierce46@gmail.com>
2014-06-25 14:54:44 -0600

    made changes to variables

--- Disemvoweler/Program.cs
+++ Disemvoweler/Program.cs
@@ -19,23 +19,28 @@ namespace Disemvoweler
         static void Disemvoweler(string input)
         {
             //hold our output
+            string vowels = "";
+            string consonants = "";
             for (int i = 0; i < input.Length; i ++)
             {
-                string vowels = "";
-                string consonants = "";
                 char letter = input[i];
                 //pull vowels out of string
-                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'I')
                 {
-                    letter.ToString();
+                    //convert letters to string
+                    vowels = vowels + letter.ToString();
                 }
                     //pull special characters out of string
                 else if (letter != ' ' && letter != '.' && letter != '?' && letter != ',' && letter != '!' && letter != '\'')
                 {
-                    letter.ToString();
+                    //convert letters to string
+                    consonants = consonants + letter.ToString();
                 }
-
+          
             }
+             //print to console
+            Console.WriteLine("Original: "+ input);
+            Console.WriteLine("Disemvoweled: "+ consonants);
         }
     }
 }
